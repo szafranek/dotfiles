3ec8cb6541b41186ceed4334bb140e51025fd772
Krzysztof Szafranek <ebraun@o2.pl>
2018-10-10 02:28:44 +0200

    Add script with installed brew packages

---
+++ brew_install.sh
@@ -0,0 +1,7 @@
+brew install diff-so-fancy bat ripgrep
+brew install ffmpeg
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package
+
+brew install pyenv pyenv-virtualenv
+
+brew install mysql httpd
