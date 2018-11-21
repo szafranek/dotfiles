35f52a94b439f80da43935a6c43f966a04a7d99d
Krzysztof Szafranek <ebraun@o2.pl>
2018-11-21 21:10:13 +0100

    Remove manual homebrew script obsoleted by Brewfile

--- brew_install.sh
+++
@@ -1,7 +0,0 @@
-brew install diff-so-fancy bat ripgrep
-brew install ffmpeg
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize suspicious-package
-
-brew install pyenv pyenv-virtualenv
-
-brew install mysql httpd
